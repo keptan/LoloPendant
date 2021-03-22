96b771c07b484bc73068dc9dda5f096458df2e75
keptan <keptan@protonmail.com>
2021-03-21 20:38:17 -0400

    trying out unperson and feeder as objects

--- index.js
+++ index.js
@@ -1,202 +1,123 @@
-const Discord = require('discord.js');
-const myIntents = new Discord.Intents(Discord.Intents.NON_PRIVILEGED);
-myIntents.add('GUILD_MEMBERS', 'GUILD_PRESENCES');
-const client = new Discord.Client({forceFetchUsers: true, ws: { intents: myIntents }});
+const Discord = require('discord.js')
+const myIntents = new Discord.Intents(Discord.Intents.NON_PRIVILEGED)
+myIntents.add('GUILD_MEMBERS', 'GUILD_PRESENCES')
+const client = new Discord.Client({forceFetchUsers: true, ws: { intents: myIntents }})
 const config = require('./config.json')
 const Markov = require('ez-markov')
 const Eater  = require('discord-fetch-all')
 
-client.login(config.token);
+client.login(config.token)
 
-var hooks = new Map();
-var generators = new Map();
-var channelsEaten = new Set();
 
-const wait = ms => new Promise(resolve => setTimeout(resolve, ms));
+function HookManager ()
+{
+	this.hooks = new Map()
 
+	this.retrieveHook =  async function (channel)
+	{
+		if(this.hooks.has(channel)) return this.hooks.get(channel)
+
+		var ret = await channel.fetchWebhooks()
+
+		if(!ret.size)
+		{
+			await channel.createWebhook('loloHook')
+		}
+
+		this.hooks.set(channel, ret.first())
+
+		return this.hooks.get(channel)
+	}
+}
+
+
+function MessageScanners ()
+{
+	this.scanners = new Set()
+
+	this.feed = function (m)
+	{
+		for (const s of this.scanners)
+		{
+			s.feed(m)
+		}
+	}
+}
 
 function MarkovKing (c)
 {
-	const channelsEaten = new Set();
-	const generators	= new Map();
+	this.client = c
+	this.channelsEaten = new Set()
+	this.generators	= new Map()
 
-	async function eatMessages (channel)
+	this.eatMessages = async function eatMessages (channel)
 	{
-		if(!channel.isText()) return;
-		if(this.channelsEaten.has(channel)) return; 
-		this.channelsEaten.add(channel);
+		if(!channel.isText()) return
+		if(this.channelsEaten.has(channel)) return
+		this.channelsEaten.add(channel)
 
-		const allMessages = await Eater.messages(channel, {userOnly: true});
+		const allMessages = await Eater.messages(channel, {userOnly: true})
 
 		for (const m of allMessages)
 		{
-			if(!this.generators.has(m.member)) this.generators.set(m.member, new Markov());
-			const gen = this.generators.get(m.member);
+			if(!this.generators.has(m.member)) this.generators.set(m.member, new Markov())
+			const gen = this.generators.get(m.member)
 			if(m.content)
 			{
-				gen.addCorpus(m.content.replace(/(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)/, ""));
+				gen.addCorpus(m.content.replace(/(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([/\w .-]*)/, ''))
 			}
 		}
 	}
 
 
-	function generateMessage ()
+	this.generateMessage = function ()
 	{
-		let index = Math.floor(Math.random() * generators.size);
-		let acc = 0;
-		var key;
+		let index = Math.floor(Math.random() * this.generators.size)
+		let acc = 0
 		for(let key of this.generators.keys())
 		{
-			if (acc++ === index) return {member: key, message: this.generators.get(key).getSentence()};
+			if (acc++ === index) return {member: key, message: this.generators.get(key).getSentence()}
 		}
 	}
-};
-
-
-
-
-
-function shuffleNames (array)
-{
-    for (var i = array.length - 1; i > 0; i--) {
-        var j = Math.floor(Math.random() * (i + 1));
-        var temp = array[i].name;
-        array[i].name = array[j].name;
-        array[j].name = temp;
-    }
 }
 
-function scrambleNames (guild)
+function Unpersonator(hooks)
 {
-	const names = new Array();
-
-	guild.members.fetch().then
-	(
-
-		guild.members.cache.each(member => 
-			{
-			names.push({member: member, name: mmber.user.username})
-			}
-	));
-
-
-	shuffleNames(names);
-
-	for(var n of names)
+	this.hooks = hooks
+	this.populateNames = async function (guild)
 	{
-		n.member.setNickname(n.name).catch(e => undefined);
+		var name, uri
+		var member = await guild.members.cache.random()
+		name = member.displayName
+		member = await guild.members.cache.random()
+		uri  = member.user.displayAvatarURL()
+
+		return {name: name, uri: uri}
+	}
+
+	this.feed = async function (m)
+	{
+		const uris = [];
+		m.attatchments.forEach(a => {uris.push(a.proxyURL)})
+
+		const member = await this.populateNames(m.guild)
+		const hook   = await this.hooks.retrieveHook(m.channel)
+		hook.send(m.content, {files: uris, attatchments: m.attatchments, username: member.name, avatarURL: member.uri})
 	}
 }
 
-function unscrambleNames (guild)
-{
-}
+const hookMan  = new HookManager()
+const unperson = new Unpersonator(hookMan)
+const scanners = new MessageScanners()
 
-function scrambleAll ()
-{
-	console.log("scrambling...");
-	client.guilds.cache.forEach(server => scrambleNames(server));
-	setTimeout(scrambleAll, Math.random() * (1600 * 1000));
-}
-
-async function retrieveHook (channel)
-{
-	if(hooks.has(channel)) return hooks.get(channel);
-
-	ret = await channel.fetchWebhooks();
-	if(!ret.size)
-	{
-		await channel.createWebhook('loloHook');
-	}
-	ret = await channel.fetchWebhooks();
-	hooks.set(channel, ret.first());
-
-	return hooks.get(channel);
-}
-
-async function populateNames (guild)
-{
-
-	var color, name, uri;
-//	await guild.members.fetch();
-	var member = await guild.members.cache.random();
-	color = member.displayHexColor;
-	name  = member.displayName;
-	member = await guild.members.cache.random();
-	uri   = member.user.displayAvatarURL();
-
-
-	return {color: color, name: name, uri: uri};
-}
-
-
-
-async function spamPost ()
-{
-	for(var channel of channelsEaten)
-	{
-
-		if(channel.name !== "hell") continue;
-	
-		setTimeout(spamPost, Math.random() * (1460 * 1000));
-		const rMessage = generateMessage();	
-		if(rMessage.message === "") return;
-
-		 retrieveHook(channel).then(h =>
-			h.send(rMessage.message, {username: rMessage.member.displayName, avatarURL: rMessage.member.user.displayAvatarURL()})).catch(e => undefined);
-
-	}
-}
-
-
-
-
-
+scanners.add(unperson)
 
 client.on('message', async message  =>  
-	{
-		if (message.webhookID) return;
-		
-		if (!generators.has(message.member))
-		{
-			generators.set(message.member, new Markov());
-		}
+{
+	scanners.feed(message)
+})
 
-		const gen = generators.get(message.member);
-		if(message.content)
-		{
-			gen.addCorpus(message.content.replace(/(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)/, ""));
-		}
-
-		if (message.content === "!test")
-		{
-			spamPost();
-			return;
-		}
-
-
-		if (message.channel.name === "hell")
-		{
-
-			var uris = [];
-			message.attachments.forEach(a => { uris.push(a.proxyURL)});
-
-			const mysteryUser =  await populateNames(message.guild);
-			console.log(mysteryUser);
-
-			retrieveHook(message.channel).then(h => 
-				h.send(message.content, {files: uris, attachments: message.attachments, color: mysteryUser.color , username: mysteryUser.name , avatarURL: mysteryUser.uri })
-				.then(wait(4000).then(message.delete())));
-		}
-
-		if( message.channel.name == "hell" && Math.random() > 0.90)
-		{
-			const rMessage = generateMessage();	
-			wait(Math.random() * (2600 * 1000)).then( () => retrieveHook(message.channel).then(h =>
-				h.send(rMessage.message, {username: rMessage.member.displayName, avatarURL: rMessage.member.user.displayAvatarURL()}))).catch(e => undefined);
-		}
-	});
-
+/*
 //setTimeout(scrambleAll, Math.random() * (1600 * 1000));
 client.on("ready", async =>
 	{
@@ -210,4 +131,5 @@ client.on("ready", async =>
 		);
 
 	});
+*/
 
