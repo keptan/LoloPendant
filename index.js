02aa381300981cc49a376c450a17577a32c696f4
mimo <mimo@debian-BULLSEYE-live-builder-AMD64>
2023-12-03 21:17:31 -0500

    Updated to new discordjs version, removed markov feature, removed harassment feature, revamped anonymizationfeature

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const Discord = require('discord.js')
-const myIntents = new Discord.Intents(Discord.Intents.NON_PRIVILEGED)
-myIntents.add('GUILD_MEMBERS', 'GUILD_PRESENCES')
-const client = new Discord.Client({forceFetchUsers: true, ws: { intents: myIntents }})
 const config = require('./config.json')
-const Markov = require('ez-markov')
-const Eater  = require('discord-fetch-all')
+
+const { Client, IntentsBitField } = require('discord.js');
+
+const myIntents = new IntentsBitField();
+myIntents.add(IntentsBitField.Flags.Guilds, IntentsBitField.Flags.GuildMessages, IntentsBitField.Flags.MessageContent, IntentsBitField.Flags.GuildPresences, IntentsBitField.Flags.GuildMembers);
+
+const client = new Client({ intents: myIntents });
 
 client.login(config.token)
 
@@ -21,19 +23,16 @@ function HookManager ()
 	{
 		if(this.hooks.has(channel)) return this.hooks.get(channel)
 
-		var ret = await channel.fetchWebhooks()
+		const fetch = await channel.fetchWebhooks()
+		const found = fetch.first()
+		if(found) return found
 
-		if(!ret.size)
-		{
-			await channel.createWebhook('loloHook')
-		}
-
-		this.hooks.set(channel, ret.first())
-
+		this.hooks.set(channel, await channel.createWebhook({name: "loloPendant"}))
+		
+		console.log("4 reutrning webhook...")
 		return this.hooks.get(channel)
 	}
 }
-
 
 function MessageScanners ()
 {
@@ -58,106 +57,6 @@ function MessageScanners ()
 	}
 }
 
-function MarkovKing (c, h)
-{
-	this.client = c
-	this.hooks = h
-
-	this.channelsEaten = new Set()
-	this.generators	= new Map()
-
-	this.eatMessages = async function eatMessages (channel)
-	{
-		if(!channel.isText()) return
-		if(this.channelsEaten.has(channel)) return
-		this.channelsEaten.add(channel)
-
-		const allMessages = await Eater.messages(channel, {userOnly: true})
-
-		for (const m of allMessages)
-		{
-			if(!this.generators.has(m.member)) this.generators.set(m.member, new Markov())
-			const gen = this.generators.get(m.member)
-			if(m.content)
-			{
-				gen.addCorpus(m.content.replace(/(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([/\w .-]*)/, ''))
-			}
-		}
-	}
-
-
-	this.generateMessage = function ()
-	{
-		let index = Math.floor(Math.random() * this.generators.size)
-		let acc = 0
-		for(let key of this.generators.keys())
-		{
-			if(Math.random() > 0.5)
-			{
-				if (acc++ === index) return {member: key, message: this.generators.get(key).getCorpus()}
-			}
-			else
-			{
-
-				if (acc++ === index) return {member: key, message: this.generators.get(key).getSentence()}
-			}
-
-		}
-	}
-
-	this.init = async function (client)
-	{
-		client.guilds.cache.forEach(async server => server.members.fetch())
-		client.guilds.cache.forEach(async server => 
-		{
-			server.channels.cache.each(c => this.eatMessages(c))
-		}
-		)
-
-	}
-
-	this.post = async function (channel)
-	{
-
-			const post = this.generateMessage()
-			const hook = await this.hooks.retrieveHook(channel)
-			hook.send(post.message, {username: post.member.displayName, avatarURL: post.member.user.displayAvatarURL()}).catch(e => undefined)
-	}
-
-
-	this.feed = async function (m)
-	{
-		const split = m.content.split(' ')
-		if(split[0] == '!rpost')
-		{
-			await this.post(m.channel)
-			if(m.channel.name !== 'hell')
-			{
-				m.delete()
-			}
-		}
-
-		if(split[0] == '!wave')
-		{
-			postWave(this, m.channel)
-		}
-	}
-
-}
-
-async function postWave (m, c)
-{
-	for(var i = 0; i < Math.random() * 10; i++)
-	{
-		m.post(c)
-		await sleep (Math.random() * 60 * 1000)
-	}
-	await sleep( Math.random() * 60 * 6 * 1000)
-	postWave(m, c)
-}
-
-
-
 function Unpersonator(hooks)
 {
 	this.hooks = hooks
@@ -174,58 +73,52 @@ function Unpersonator(hooks)
 
 	this.feed = async function (m)
 	{
-		if(m.webhookID) return 
-		if(m.channel.name !== "hell") return
-		const uris = [];
-		m.attachments.forEach(a => {uris.push(a.proxyURL)})
-		if(uris.length) await sleep(1000)
+		if(m.webhookId)
+		{
+			console.log("exiting feed because its recursive from a webhook or whatever")
+			return 
+		}
+		if(m.channel.name !== "anon") return
+		
+		files = []
+		embeds = []
+		if(m.attachments)
+		{
+			//await sleep(1000)
+		}
+
+		for( [k, i] of m.attachments)
+		{
+			files.push(Discord.AttachmentBuilder.from(i))
+		}
+
+		for( [k, i] of m.embeds)
+		{
+			embeds.push(Discord.EmbedBuilder.from(i))
+		}
 
 		const member = await this.populateNames(m.guild)
 		const hook   = await this.hooks.retrieveHook(m.channel)
-		hook.send(m.content, {files: uris, attatchments: m.attatchments, username: member.name, avatarURL: member.uri}).catch(e => undefined)
+	//	hook.send(m.content, {files: uris, attatchments: m.attatchments, username: member.name, avatarURL: member.uri}).catch(e => undefined)
+		hook.send({
+			content: m.content, 
+			username: member.name, 
+			avatarURL: member.uri,
+			files: files,
+			embeds: embeds,
+		})
 		m.delete()
-	}
-}
-
-function Harass ()
-{
-	this.targets = new Map()
-
-	this.add = function (s)
-	{
-		const split = s.split(' ')
-		if(split[0] === '!harass') this.targets.set(split[1], split[2])
-		if(split[0] === '!unharass') this.targets.delete(split[1])
-	}
-
-	this.feed = function (m)
-	{
-		this.add(m.content)
-		if(this.targets.has(m.author.id))
-		{
-			m.react(this.targets.get(m.author.id))
-		}
 	}
 }
 
 const hookMan  = new HookManager()
 const unperson = new Unpersonator(hookMan)
 const scanners = new MessageScanners()
-const markov   = new MarkovKing(client, hookMan)
-const perv	   = new Harass()
-
 
 scanners.add(unperson)
-scanners.add(perv)
-scanners.add(markov)
 
-client.on('message', async message  =>  
+client.on('messageCreate', async message  =>  
 {
 	scanners.feed(message)
 })
-
-//setTimeout(scrambleAll, Math.random() * (1600 * 1000));
-client.on('ready', async =>
-	markov.init(client)
-)
 
